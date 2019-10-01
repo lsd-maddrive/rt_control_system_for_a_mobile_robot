f3e99ee75a9959fa1b94a22e8ca3db9d4f2deeb8
ponomarevda <ponomarevda96@gmail.com>
2019-10-02 00:39:35 +0300

    [feature] update startup for dwa_local_planner pkg

--- robot_software/startup/install_ros_packages.sh
+++ robot_software/startup/install_ros_packages.sh
@@ -4,4 +4,5 @@ sudo apt install ros-$ROS_DISTRO-gmapping \
 		ros-$ROS_DISTRO-rosserial-python \
 		ros-$ROS_DISTRO-turtlebot3 \
 		ros-$ROS_DISTRO-turtlebot3-gazebo \
+		ros-$ROS_DISTRO-dwa-local-planner \
 		ros-$ROS_DISTRO-move-base
