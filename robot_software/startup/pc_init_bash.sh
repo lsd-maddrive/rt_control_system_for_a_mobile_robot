a574b519a73d39f0b61aae58d291d2280018aef6
ponomarevda <ponomarevda96@gmail.com>
2019-10-03 18:39:15 +0300

    [issue] fix bug in ROS_MASTER_URI and ROS_IP startups scripts

--- robot_software/startup/pc_init_bash.sh
+++ robot_software/startup/pc_init_bash.sh
@@ -2,11 +2,6 @@
 
 echo "Script pc_init_bash.sh has been runned:"
 
-echo "ROS_MASTER_URI was" $ROS_MASTER_URI "."
-echo "ROS_IP was" $ROS_IP "."
-
+export LC_NUMERIC="en_US.UTF-8"
 export ROS_MASTER_URI=http://ubuntu-desktop.local:11311
-export ROS_IP=$HOSTNAME
-
-echo "ROS_MASTER_URI is" $ROS_MASTER_URI "now."
-echo "ROS_IP is" $ROS_IP "now."
+export ROS_IP=192.168.1.21
