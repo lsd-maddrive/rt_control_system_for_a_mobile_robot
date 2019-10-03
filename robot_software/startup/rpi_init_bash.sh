c94b40692a840c60f9c423f7377d0c2168abb333
ponomarevda <ponomarevda96@gmail.com>
2019-10-03 18:31:53 +0300

    [feature] add ROS_MASTER_URI and ROS_IP startups scripts

---
+++ robot_software/startup/rpi_init_bash.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Script rpi_init_bash.sh has been runned:"
+
+echo "ROS_MASTER_URI was" $ROS_MASTER_URI "."
+echo "ROS_IP was" $ROS_IP "."
+
+export ROS_MASTER_URI=http://ubuntu-desktop.local:11311
+export ROS_IP=$HOSTNAME
+
+echo "ROS_MASTER_URI is" $ROS_MASTER_URI "now."
+echo "ROS_IP is" $ROS_IP "now."
