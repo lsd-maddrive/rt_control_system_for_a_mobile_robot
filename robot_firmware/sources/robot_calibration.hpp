369c846919b1083cfd22fe07db21456637e8c74b
ponomarevda <ponomarevda96@gmail.com>
2019-11-09 15:37:07 +0300

    [firmware] fix PI constants

--- robot_firmware/sources/robot_calibration.hpp
+++ robot_firmware/sources/robot_calibration.hpp
@@ -15,10 +15,18 @@ static constexpr float WHEELTRACK = 0.23;
 
 /// PID constants
 static constexpr float P_DEFAULT = 0.025;
-static constexpr float I_DEFAULT = 1;
+static constexpr float I_DEFAULT = 0.25;
 static constexpr float D_DEFAULT = 0;
 
-/// PID constants
+// Robot Configuration Parameters
+static constexpr float MAX_ABS_LINEAR_SPEED = 0.25;
+static constexpr float MIN_ABS_LINEAR_SPEED = 0.05;
+
+static constexpr float MAX_ABS_ANGULAR_SPEED = 1.57;
+static constexpr float MIN_ABS_ANGULAR_SPEED = 0.05;
+
+static constexpr float MIN_ABS_WHEEL_SPEED = MIN_ABS_LINEAR_SPEED;
+static constexpr float MAX_ABS_WHEEL_SPEED = MAX_ABS_ANGULAR_SPEED;
 
 
 #endif /* ROBOT_CALIBRATION_HPP */
