e4c72492f19ed7338f7042903cf61e9b17e8c2be
ponomarevda <ponomarevda96@gmail.com>
2019-11-17 15:03:26 +0300

    fix long wait for data collection from a large number of process

--- robot_software/scripts/test_cpu_load.py
+++ robot_software/scripts/test_cpu_load.py
@@ -12,98 +12,128 @@ import argparse
 THIS_NODE_NAME = 'test_cpu_load_node'
 FILE_NAME_BASE = "test_cpu_load_result_"
 FILE_NAME_END = ".json"
-
 class Json:
-    PID_DATA_NAME = 'cpu load'
-    TOTAL_CPU_LOAD = 'total cpu load'
+    """ 
+        Json structure:
+        {
+            <pid_0_name>: {<CPU_LOAD_FIELD>: list()}
+            ...
+            <pid_n_name>: {<CPU_LOAD_FIELD>: list()}
+            <total_name>: {<CPU_LOAD_FIELD>: list()}
+            <TIME>: list()
+        }
+    """
+    CPU_LOAD_FIELD = 'cpu load'
+    MEMORY_FIELD = 'memory'
+    TOTAL_NAME = 'total'
     TIME = 'time'
 
-GATHERING_TIME = 3
-TIME_FOR_SLEEP = 0.5
-TIME_AMOUNT = 60
+
+# Choose parameters
+DESIRED_PROCESS_NAMES = list(['rosserial_python'])
+TIME_FOR_SLEEP = 1
+TIME_AMOUNT = 15
+KERNEL_AMOUNT = 4
+
 
 rospy.init_node(THIS_NODE_NAME)
 
-def gatherData():
-    # Define pids
-    desired_pid_names = list(['rosserial_python'])
-    pids = dict()
-    for desired_pid_name in desired_pid_names:
-        f = os.popen('pgrep -d, -f ' + desired_pid_name)
-        output = f.read().split(',')
-        print("process {} with pid {}".format(desired_pid_name, output))
-	if len(output) > 1:
-	    new_pid_number = int(output[0])
-            data = dict([ ('pid_number', new_pid_number), ('data', list()) ])
-            pids[desired_pid_name] = data
+class DataCollector:
+    def __init__(self):
+        self.pids = dict()
+        self.time = list()
+        self.total_cpu_load = list()
 
-    # Gather data
-    data = list()
-    time = list()
-    startTime = rospy.get_rostime()
-    for i in range(0, int(TIME_AMOUNT / (GATHERING_TIME * len(pids) + TIME_FOR_SLEEP) + 1)):
-        currentTime = rospy.get_rostime()
-        relativeSecs = currentTime.secs - startTime.secs
-        relativeNsecs = currentTime.nsecs - startTime.nsecs
-        relativeTime = relativeSecs + float(relativeNsecs)/1000000000
-        time.append(relativeTime)
-        print("\nTime: {}/{}".format(relativeTime, TIME_AMOUNT))
+    def process_collection(self):
+        self.__init_pids()
+        self.__collect_data()
+        return self.__write_to_new_file()
 
-        cpu_load = psutil.cpu_percent()
-        data.append(cpu_load)
-        print("Total: {}%".format(cpu_load))
-
-        for pid_name in pids:
-            pid = psutil.Process(pids[pid_name]['pid_number'])
-	    pid_load = pid.cpu_percent(GATHERING_TIME)
-            pids[pid_name]['data'].append(pid_load)
-            print("- {} with pid {}: {}%".format(pid_name, pids[pid_name]['pid_number'], pid_load))
+    def __init_pids(self):
+        self.pids = dict()
+        print("User chose following process monitoring:")
+        for desired_pid_name in DESIRED_PROCESS_NAMES:
+            f = os.popen('pgrep -d, -f ' + desired_pid_name)
+            output = f.read().split(',')
+            if len(output) > 1:
+                new_pid_number = int(output[0])
+                pid = psutil.Process(new_pid_number)
+                pid.cpu_percent()
+                data = dict([ ('pid_number', new_pid_number), ('data', list()), ('obj', pid) ])
+                self.pids[desired_pid_name] = data
+                print("- process {} with possible pids {}".format(desired_pid_name, output))
+            else:
+                print("- process {} does not exist".format(desired_pid_name))
+        print("Result process and their pid which exist:")
+        for key in self.pids:
+            print("- process {} with pid {}".format(key, self.pids[key]['pid_number']))
         rospy.sleep(TIME_FOR_SLEEP)
 
-    # Write file
-    counter = 0
-    while os.path.isfile(FILE_NAME_BASE + str(counter) + FILE_NAME_END) is not False:
-        counter += 1
-    fileName = FILE_NAME_BASE + str(counter) + FILE_NAME_END
-    write_file = open(fileName, "w")
-    new_dump = dict([("time", time),
-                     (Json.TOTAL_CPU_LOAD, data)])
-    for pid_name in pids:
-        pid = pids[pid_name]
-        data = pid['data']
-        new_dump[pid_name] = dict([('data', data)])
-    json.dump(new_dump, write_file, indent=2)
-    print("File name is" + str(fileName))
-    
-    return fileName
+    def __collect_data(self):
+        startTime = rospy.get_rostime()
+        for i in range(0, int(TIME_AMOUNT / TIME_FOR_SLEEP) + 1):
+            currentTime = rospy.get_rostime()
+            relativeSecs = currentTime.secs - startTime.secs
+            relativeNsecs = currentTime.nsecs - startTime.nsecs
+            relativeTime = relativeSecs + float(relativeNsecs)/1000000000
+            self.time.append(relativeTime)
+            print("\nTime: {}/{}".format(relativeTime, TIME_AMOUNT))
 
-def createPlot(fileName):
-    # Read file
+            cpu_load = psutil.cpu_percent()
+            self.total_cpu_load.append(cpu_load)
+            print("Total: {}%".format(cpu_load))
+
+            for pid_name in self.pids:
+                pid = self.pids[pid_name]['obj']
+                pid_load = pid.cpu_percent() / KERNEL_AMOUNT
+                self.pids[pid_name]['data'].append(pid_load)
+                print("- {} with pid {}: {}%".format(pid_name, self.pids[pid_name]['pid_number'], pid_load))
+            rospy.sleep(TIME_FOR_SLEEP)
+
+    def __write_to_new_file(self):
+        counter = 0
+        while os.path.isfile(FILE_NAME_BASE + str(counter) + FILE_NAME_END) is not False:
+            counter += 1
+        fileName = FILE_NAME_BASE + str(counter) + FILE_NAME_END
+        write_file = open(fileName, "w")
+
+        new_dump = dict([ (Json.TIME, self.time),
+                          (Json.TOTAL_NAME, dict([(Json.CPU_LOAD_FIELD, self.total_cpu_load)])) ])
+        for pid_name in self.pids:
+            pid = self.pids[pid_name]
+            pid_cpu_load = pid['data']
+            new_dump[pid_name] = dict([(Json.CPU_LOAD_FIELD, pid_cpu_load)])
+
+        json.dump(new_dump, write_file, indent=2)
+        print("File name is {}".format(fileName))
+        return fileName
+
+def create_plot(fileName):
+    # Read file and check correctness
     read_file = open(fileName, "r")
     dump = json.load(read_file)
-    time = dump["time"]
-    total_cpu_data = dump[Json.TOTAL_CPU_LOAD]
+    if Json.TIME in dump:
+        time = dump[Json.TIME]
+    else:
+        print("Json parse error: there is no time data!")
+        return
 
     # Show data
     for key in dump:
         print("{} is {}.".format(key, dump[key]))
 
-    # Create plot
-    #for i in range(1, len())
-    #plt.subplot(2, 2, i)
-    if 'time' in dump:
-        if Json.TOTAL_CPU_LOAD in dump:
-            plt.subplot(2, 2, 1)
-            plt.title(Json.TOTAL_CPU_LOAD)
-            plt.xlabel("{}, sec".format(Json.TIME))
-            plt.ylabel("{}%".format(Json.TOTAL_CPU_LOAD))
-            plt.plot(time, dump[Json.TOTAL_CPU_LOAD])
-            plt.grid()
+    # Create plot and show parsed data
+    for key in dump:
+        if key == Json.TIME:
+            continue
         else:
-            print("Json parse error: there is no cpu load data!")
-        plt.show()
-    else:
-        print("Json parse error: there is no time data!")
+            print("- {} was founded in json.".format(key))
+            plt.plot(time, dump[key][Json.CPU_LOAD_FIELD])
+    plt.title('cpu load')
+    plt.xlabel("{}, sec".format(Json.TIME))
+    plt.ylabel("{}%".format('Cpu load'))
+    plt.grid()
+    plt.show()
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='Test cpu load tool')
@@ -119,7 +149,8 @@ if __name__=="__main__":
     try:
         filePath = args['jdir']
         if args['mode'] == "gather_and_plot":
-            filePath = gatherData()
-        createPlot(filePath)
+            data_collector = DataCollector()
+            filePath = data_collector.process_collection()
+        create_plot(filePath)
     except (rospy.ROSInterruptException, KeyboardInterrupt):
         rospy.logerr('Exception catched')
