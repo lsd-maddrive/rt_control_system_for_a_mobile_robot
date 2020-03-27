df3162eaa03d2dc45ee7c8b0f8c534f22ae25790
ponomarevda <ponomarevda96@gmail.com>
2020-03-28 00:54:34 +0300

    [software] add ability to remote spawn of model

---
+++ robot_software/scripts/delete_model.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import rospy
+from gazebo_msgs.srv import DeleteModel
+
+def delete_model(model_name):
+    proxy = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
+    proxy(model_name)
+
+
+if __name__=="__main__":
+    try:
+        rospy.init_node('delete_model_node')
+        delete_model("turtlebot3_burger")
+    except (rospy.ROSInterruptException, KeyboardInterrupt):
+        rospy.logerr('Exception catched')
