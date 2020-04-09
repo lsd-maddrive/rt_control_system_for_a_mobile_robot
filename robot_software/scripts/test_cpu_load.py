c497a699fc831472a8c2cdc6911efe7889b6553f
ponomarevda <ponomarevda96@gmail.com>
2020-04-09 18:56:29 +0300

    [software] add args and pids to cpu load tool

--- robot_software/scripts/test_cpu_load.py
+++ robot_software/scripts/test_cpu_load.py
@@ -29,16 +29,12 @@ class Json:
     TOTAL_NAME = 'total'
     TIME = 'time'
 
-
-# Choose parameters
-DESIRED_PROCESS_NAMES = list(['rosserial_python'])
-TIME_FOR_SLEEP = 1
-TIME_AMOUNT = 15
-KERNEL_AMOUNT = 4
-LANGUAGE = 'rus'
-
-
-rospy.init_node(THIS_NODE_NAME)
+class Config(object):
+    LANGUAGE = 15
+    TIME_AMOUNT = 15
+    TIME_FOR_SLEEP = 15
+    DESIRED_PROCESS_NAMES = list(['rosserial_python', 'gzserver', 'move_base', 'slam_gmapping', 'hector_mapping'])
+    KERNEL_AMOUNT = 4
 
 class DataCollector:
     def __init__(self):
@@ -54,32 +50,32 @@ class DataCollector:
     def __init_pids(self):
         self.pids = dict()
         print("User chose following process monitoring:")
-        for desired_pid_name in DESIRED_PROCESS_NAMES:
+        for desired_pid_name in Config.DESIRED_PROCESS_NAMES:
             f = os.popen('pgrep -d, -f ' + desired_pid_name)
-            output = f.read().split(',')
-            if len(output) > 1:
-                new_pid_number = int(output[0])
+            new_pids_numbers = f.read().split(',')
+            if len(new_pids_numbers) > 1:
+                new_pid_number = int(new_pids_numbers[0])
                 pid = psutil.Process(new_pid_number)
                 pid.cpu_percent()
                 data = dict([ ('pid_number', new_pid_number), ('data', list()), ('obj', pid) ])
                 self.pids[desired_pid_name] = data
-                print("- process {} with possible pids {}".format(desired_pid_name, output))
+                print("- process {} with possible pids {}".format(desired_pid_name, new_pids_numbers))
             else:
                 print("- process {} does not exist".format(desired_pid_name))
         print("Result process and their pid which exist:")
         for key in self.pids:
             print("- process {} with pid {}".format(key, self.pids[key]['pid_number']))
-        rospy.sleep(TIME_FOR_SLEEP)
+        rospy.sleep(Config.TIME_FOR_SLEEP)
 
     def __collect_data(self):
         startTime = rospy.get_rostime()
-        for i in range(0, int(TIME_AMOUNT / TIME_FOR_SLEEP) + 1):
+        for i in range(0, int(Config.TIME_AMOUNT / Config.TIME_FOR_SLEEP) + 1):
             currentTime = rospy.get_rostime()
             relativeSecs = currentTime.secs - startTime.secs
             relativeNsecs = currentTime.nsecs - startTime.nsecs
             relativeTime = relativeSecs + float(relativeNsecs)/1000000000
             self.time.append(relativeTime)
-            print("\nTime: {}/{}".format(relativeTime, TIME_AMOUNT))
+            print("\nTime: {}/{}".format(relativeTime, Config.TIME_AMOUNT))
 
             cpu_load = psutil.cpu_percent()
             self.total_cpu_load.append(cpu_load)
@@ -87,10 +83,10 @@ class DataCollector:
 
             for pid_name in self.pids:
                 pid = self.pids[pid_name]['obj']
-                pid_load = pid.cpu_percent() / KERNEL_AMOUNT
+                pid_load = pid.cpu_percent() / Config.KERNEL_AMOUNT
                 self.pids[pid_name]['data'].append(pid_load)
                 print("- {} with pid {}: {}%".format(pid_name, self.pids[pid_name]['pid_number'], pid_load))
-            rospy.sleep(TIME_FOR_SLEEP)
+            rospy.sleep(Config.TIME_FOR_SLEEP)
 
     def __write_to_new_file(self):
         counter = 0
@@ -135,11 +131,11 @@ def create_plot(fileName):
     TITLE = ''
     X_LABEL = ''
     Y_LABEL = ''
-    if LANGUAGE is 'rus':
+    if Config.LANGUAGE is 'rus':
         TITLE = u'загрузка процессора'
         X_LABEL = u'время, сек'
         Y_LABEL = u'загрузка процессора, %'
-    if LANGUAGE is 'eng':
+    if Config.LANGUAGE is 'eng':
         TITLE = 'cpu load'
         X_LABEL = 'time, sec'
         Y_LABEL = 'cpu load, %'
@@ -151,21 +147,42 @@ def create_plot(fileName):
     plt.show()
 
 if __name__=="__main__":
-    parser = argparse.ArgumentParser(description='Test cpu load tool')
+    parser = argparse.ArgumentParser(description=
+        """
+        \rMonitor cpu load tool.
+        \rTwo types of usage:
+        \r1. Only plot data (default mode, you should choose the file):
+        \rrosrun robot_software test_cpu_load.py --jdir test_results/test1
+        \r2. Collect data and plot them (name of file will be generated):
+        \rrosrun robot_software test_cpu_load.py --mode collect_and_plot 
+        """,
+        formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('--jdir',
                         type=str,
-                        help='Default JSON dir',
-                        default=str())
+                        help='JSON directory')
     parser.add_argument('--mode', 
                         help='Mode: [only_plot, collect_and_plot]',
                         default='only_plot')
+    parser.add_argument('--language', 
+                        help='Variants: [rus, eng]',
+                        default='rus')
+    parser.add_argument('--time', 
+                        help='Time of cpu usage monitoring',
+                        default=15)
+    parser.add_argument('--interval', 
+                        help='Interval between cpu measurements',
+                        default=1)
     args = vars(parser.parse_args())
 
     try:
-        filePath = args['jdir']
+        rospy.init_node(THIS_NODE_NAME)
+        file_path = args['jdir']
+        Config.LANGUAGE = args['language']
+        Config.TIME_AMOUNT = float(args['time'])
+        Config.TIME_FOR_SLEEP = float(args['interval'])
         if args['mode'] == "collect_and_plot":
             data_collector = DataCollector()
-            filePath = data_collector.process_collection()
-        create_plot(filePath)
+            file_path = data_collector.process_collection()
+        create_plot(file_path)
     except (rospy.ROSInterruptException, KeyboardInterrupt):
         rospy.logerr('Exception catched')
