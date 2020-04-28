c3cf4b7ddae486f7f073318d33a5761989a83aee
Dima <ponomarevda96@gmail.com>
2020-04-29 02:32:29 +0300

    [dc motor simulation] add fuzzy regulator and optimize pid regulator

--- matlab/diploma_dc_motor.m
+++ matlab/diploma_dc_motor.m
@@ -20,32 +20,80 @@ L_ya = 0.01                     % ?
 
 
 % Constants
-SIM_TIME = 1.0;
+SIM_TIME = .1;
+STEP_SIZE = 0.001;
 RPM_TO_TPS = 200/60;            % rotation per minutes to ticks per second
 TICK_TO_METER = 0.0005167;      % calibration constant
 METER_TO_TICK = 1/0.0005167;    % calibration constant
 DESIRED_SPEED = 0.15
-kp = 0.025
-ki = 0.25
-sim('dc_motor')
+% kp = 0.025                    % default
+% ki = 0.25                     % default
+% kd = 0                        % default
+kp = 0.0850                     % optimized
+ki = 8.42                       % optimized
+kd = 0.0001340                  % optimized
+fuzzy_regulator = readfis('fuzzy_regulator.fis');
 
-figure
-plot(speed_of_revolutions.time, speed_of_revolutions.signals.values)
-title('speed of revolution, rpm')
-grid()
+% FIS = mamfis;
+% FIS = addInput(FIS,[-500 500],'Name','E');
+% FIS = addMF(FIS,'E','trimf',[-1000 -10 0],'Name','Negative');
+% FIS = addMF(FIS,'E','trimf',[-10 0 10],'Name','Zero');
+% FIS = addMF(FIS,'E','trimf',[0 10 20],'Name','Positive');
 
-figure
-subplot(1, 3, 1)
-plot(speed.time, speed.signals.values)
+
+for i = 0:1
+    if i == 0
+        FZ_INSTEAD_PID = 1;
+        sim('dc_motor')
+        result_plot = figure;
+    elseif i == 1
+        FZ_INSTEAD_PID = 0;
+        sim('dc_motor')
+    end
+    subplot(2, 3, 1)
+    hold on
+    plot(speed.time, speed.signals.values)
+    
+    subplot(2, 3, 2)
+    hold on
+    plot(encoder.time, encoder.signals.values)
+
+    subplot(2, 3, 3)
+    hold on
+    plot(voltage.time, voltage.signals.values)
+    
+    subplot(2, 3, 4)
+    hold on
+    plot(fuzzy_data.time, fuzzy_data.signals.values)
+    
+    subplot(2, 3, 5)
+    hold on
+    plot(speed_of_revolutions.time, speed_of_revolutions.signals.values)
+end
+
+% Add info to plots
+subplot(2, 3, 1)
+hold on
+plot([0 speed.time(end)], [DESIRED_SPEED DESIRED_SPEED])
 title('speed, m/sec')
-grid()
+legend('fuzzy', 'pid')
+grid on
 
-subplot(1, 3, 2)
-plot(encoder.time, encoder.signals.values)
+subplot(2, 3, 2)
+hold on
 title('encoder, ticks')
-grid()
+legend('fuzzy', 'pid')
+grid on
 
-subplot(1, 3, 3)
-plot(voltage.time, voltage.signals.values)
+subplot(2, 3, 3)
+hold on
 title('PWM, duty cycle')
-grid()
+legend('fuzzy', 'pid')
+grid on
+
+subplot(2, 3, 4)
+legend('difference', 'derivative', 'integral', 'out')
+
+subplot(2, 3, 5)
+title('speed of revolution, rpm')
+legend('fuzzy', 'pid')
