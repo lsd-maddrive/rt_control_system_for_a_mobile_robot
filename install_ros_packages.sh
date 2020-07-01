c1022620883f9ab58f1b6cb84cdfcb5b44573c76
ponomarevda <ponomarevda96@gmail.com>
2020-07-01 09:34:54 +0300

    fix tutorial, update requirements and fix dependencies

--- install_ros_packages.sh
+++ install_ros_packages.sh
@@ -5,7 +5,8 @@ sudo apt install ros-$ROS_DISTRO-gmapping \
 				 ros-$ROS_DISTRO-turtlebot3 \
 				 ros-$ROS_DISTRO-turtlebot3-gazebo \
 				 ros-$ROS_DISTRO-dwa-local-planner \
-				 ros-$ROS_DISTRO-move-base
+				 ros-$ROS_DISTRO-move-base \
+				 libsuitesparse-dev 					# for karto_slam
 
 git -C slam_karto/sparse_bundle_adjustment pull || git clone https://github.com/ros-perception/sparse_bundle_adjustment slam_karto/sparse_bundle_adjustment
 git -C slam_karto/slam_karto pull               || git clone https://github.com/ros-perception/slam_karto slam_karto/slam_karto
--- startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-. robot_software/startup/install_ros_packages.sh
-. robot_software/startup/init_stm32_and_ydlidar.sh
-
